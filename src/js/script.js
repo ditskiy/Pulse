da50b25e5f65eadff7d529f229f62b9d7e9f0897
DimaWork <dditskiy@gmail.com>
2023-01-30 17:57:30 +0200

    add modal and js modal

--- src/js/script.js
+++ src/js/script.js
@@ -31,7 +31,7 @@ const slider = tns({
   responsive: {
     320: {
       nav: true,
-    //   navPosition: "bottom",
+      //   navPosition: "bottom",
     },
     1025: {
       nav: false,
@@ -45,28 +45,57 @@ document.querySelector(".next").onclick = function () {
   slider.goTo("next");
 };
 
-(function($) {
-  $(function() {
-    
-    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tabs_active)', function() {
-      $(this)
-        .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
-        .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
-    });
+(function ($) {
+  $(function () {
+    $("ul.catalog__tabs").on(
+      "click",
+      "li:not(.catalog__tabs_active)",
+      function () {
+        $(this)
+          .addClass("catalog__tab_active")
+          .siblings()
+          .removeClass("catalog__tab_active")
+          .closest("div.container")
+          .find("div.catalog__content")
+          .removeClass("catalog__content_active")
+          .eq($(this).index())
+          .addClass("catalog__content_active");
+      }
+    );
 
     function toggleSlide(item) {
-      $(item).each(function(i) {
-        $(this).on('click', function(e) {
+      $(item).each(function (i) {
+        $(this).on("click", function (e) {
           e.preventDefault();
-          $('catalog-item_content').eq(i).toggleClass('catalog-item_content_active')
-          ;
-          $('.catalog-item__link').eq(i).toggleClass('.catalog-item__list_active');
-        })
+          $("catalog-item_content")
+            .eq(i)
+            .toggleClass("catalog-item_content_active");
+          $(".catalog-item__link")
+            .eq(i)
+            .toggleClass(".catalog-item__list_active");
+        });
       });
-    };
-  
-    toggleSlide('.catalog-item__link');
-    toggleSlide('.catalog-item__back');
+    }
 
+    toggleSlide(".catalog-item__link");
+    toggleSlide(".catalog-item__back");
+
+    // modal
+
+    $("[data-modal=consultation]").on("click", function () {
+      $(".overlay, #consultation").fadeIn("slow");
+    });
+    $(".modal__close").on("click", function () {
+      $(".overlay, #consultation, #order, #thanks").fadeOut("slow");
+    });
+
+    $(".button_mini").each(function (i) {
+      $(this).on("click", function () {
+        $("#order .model__descr").text(
+          $(".catalog-item__subtitle").eq(i).text()
+        );
+        $(".overlay, #order").fadeIn("slow");
+      });
+    });
   });
-  })(jQuery);
+})(jQuery);
